05b4c984644517276d68d7317520cd2d234a6fde
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-08-18 23:05:59 +0800

    Accepting parameters of enum GatewayIntents

--- democord/intents.py
+++ democord/intents.py
@@ -1,32 +1,16 @@
+from .types import GatewayIntents
 from typing import Self
 
 
 class Intents:
   def __init__(
     self,
-    guilds : bool = False,
-    members : bool = False,
-    moderation : bool = False,
-    emojis_and_stickers : bool = False,
-    webhooks : bool = False,
-    integrations : bool = False,
-    invites : bool = False,
-    voice_states : bool = False,
-    presences : bool = False,
-    messages : bool = False,
-    reactions : bool = False,
-    typing : bool = False,
-    direct_messages : bool = False,
-    direct_message_reactions : bool = False,
-    direct_message_typing : bool = False,
-    message_content : bool = False,
-    scheduled_events : bool = False,
-    auto_moderation_configuration : bool = False,
-    auto_moderation_execution : bool = False,
-    guild_polls : bool = False,
-    dm_polls : bool = False
+    *intents
   ) -> None:
-    pass
+    self.value : int = 0
+    for intent in intents:
+      assert isinstance(intent, GatewayIntents), "Arguments passed must be a GatewayIntents enum"
+      self.value |= intent.value
 
 
   @classmethod
