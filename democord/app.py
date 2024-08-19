4fc81f12719ef58bf77829bc84941bbb2d72318d
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-08-19 16:39:08 +0800

    Added democord.App.intents attribute

--- democord/app.py
+++ democord/app.py
@@ -6,10 +6,23 @@ from .ws       import DiscordWebSocket
 class App:
   def __init__(
     self,
-    token : str = None
+    token : str = None,
+    *,
+    intents : Intents | None = None
   ) -> None:
     self.__token : str = token
-    self.ws : DiscordWebSocket = DiscordWebSocket(self)
+    self._ws : DiscordWebSocket = DiscordWebSocket(self)
+    self._intents : Intents = intents if intents else Intents.none()
+
+
+  @property
+  def intents(self) -> None:
+    return self._intents.value
+
+
+  @property
+  def ws(self) -> DiscordWebSocket:
+    return self._ws
 
 
   def run(self) -> None:
