9519e259d2cb6b2d0d05d3d6c63cf02160b2f718
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-08-19 19:07:00 +0800

    Added docstrings and finalized

--- democord/intents.py
+++ democord/intents.py
@@ -3,10 +3,24 @@ from typing import Self
 
 
 class Intents:
+  """
+  Holds the intents the app is going to use.
+
+  Attributes:
+    value (int): The corresponding bitfield integer of all the passed intents.
+  """
+
+
   def __init__(
     self,
     *intents
   ) -> None:
+    """
+    Initialize an Intents instance
+
+    Parameters:
+      intents (Union[GatewayIntents]): Gateway intents to pass for the bot.
+    """
     self._value : int = 0
     for intent in intents:
       assert isinstance(intent, GatewayIntents), "Arguments passed must be a GatewayIntents enum"
@@ -31,9 +45,21 @@ class Intents:
 
   @property
   def value(self) -> int:
+    """
+    The corresponding bitfield integer of all the passed intents.
+
+    Returns:
+      int: The corresponding bitfield integer of all the passed intents.
+    """
     return self._value
 
 
   @classmethod
   async def none(cls) -> Self:
+    """
+    Creates an Intents instance with no passed intent.
+
+    Returns:
+      Intents: An Intents instance with no passed intent.
+    """
     return cls()
