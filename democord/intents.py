ecd9e21bf55ee35efe9237b5922a29218c7ca538
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-08-19 16:48:57 +0800

    democord.Intents : __invert__ and __and__ dunder methods

--- democord/intents.py
+++ democord/intents.py
@@ -7,10 +7,25 @@ class Intents:
     self,
     *intents
   ) -> None:
-    self.value : int = 0
+    self._value : int = 0
     for intent in intents:
       assert isinstance(intent, GatewayIntents), "Arguments passed must be a GatewayIntents enum"
-      self.value |= intent.value
+      if self._value & intent.value: continue
+      self._value |= intent.value
+
+
+  def __invert__(self, other : Intents) -> Self:
+    return ~self._value
+
+
+  def __and__(self, other : Intents) -> Self:
+    self._value |= ~other
+    return self._value
+
+
+  @property
+  def value(self) -> int:
+    return self._value
 
 
   @classmethod
