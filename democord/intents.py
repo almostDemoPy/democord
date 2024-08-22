483b008651626f1dd55204b65e1e7ddf6929a79b
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-08-22 14:47:39 +0800

    democord.Intents.default() : All intents are passed except for privileged ones

--- democord/intents.py
+++ democord/intents.py
@@ -2,7 +2,7 @@ from .types import GatewayIntents
 from typing import Self
 
 
-class Intents:
+class Intents(object):
   """
   Holds the intents the app is going to use.
 
@@ -23,24 +23,27 @@ class Intents:
     """
     self._value : int = 0
     for intent in intents:
-      assert isinstance(intent, GatewayIntents), "Arguments passed must be a GatewayIntents enum"
-      if self._value & intent.value: continue
+      assert isinstance(intent, GatewayIntents), f"Arguments passed must be a GatewayIntents enum, not {type(intent)}"
+      if ( self._value & intent.value ) == intent.value: continue
       self._value |= intent.value
 
 
   def __ior__(self, intent : GatewayIntents) -> Self:
-    if self._value & intent.value: return self
+    if not isinstance(intent, GatewayIntents): raise TypeError(f"Type must be GatewayIntents, not {type(intent)}")
+    if ( self._value & intent.value ) == self._value: return self
     self._value |= intent.value
     return self
 
 
   def __iand__(self, intent : GatewayIntents) -> Self:
+    if not isinstance(intent, GatewayIntents): raise TypeError(f"Type must be GatewayIntents, not {type(intent)}")
     self._value &= ~intent.value
     return self
 
 
   def __and__(self, intent : GatewayIntents) -> bool:
-    return self._value & intent.value
+    if not isinstance(intent, GatewayIntents): raise TypeError(f"Must be GatewayIntents, not {type(intent)}")
+    return ( self._value & intent.value ) == intent.value
 
 
   @property
@@ -55,11 +58,20 @@ class Intents:
 
 
   @classmethod
+  def default(cls) -> Self:
+    """
+    Creates an Intents instance with all, except the privileged ones, intents enabled.
+
+    Returns: Intents
+    """
+    return cls(*[intent for intent in GatewayIntents if intent.name not in ["message_content", "presences", "members"]])
+
+
+  @classmethod
   def none(cls) -> Self:
     """
     Creates an Intents instance with no passed intent.
 
-    Returns:
-      Intents: An Intents instance with no passed intent.
+    Returns: Intents
     """
     return cls()
