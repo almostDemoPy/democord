a0c27601e4c9e65473b4bf8b8e52d116ba9314a9
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-08-23 15:06:51 +0800

    democord.User

---
+++ democord/user.py
@@ -0,0 +1,26 @@
+class User:
+  def __init__(
+    self,
+    data : dict[str, str | bool | int | dict | None]
+  ) -> None:
+    print(data)
+    for attribute in data:
+      self.__dict__[attribute] = data[attribute]
+
+
+  def __getattribute__(self, attribute : str) -> str | int | bool | None:
+    match attribute:
+      case "global name":
+        global_name : str | None = super().__getattribute__(attribute)
+        if not global_name: global_name : str = self.username
+        return global_name
+      case _: return super().__getattribute__(attribute)
+
+
+  def __getattr__(self, attribute : str) -> None:
+    raise AttributeError(f"User object has no attribute: {attribute}")
+
+
+  @classmethod
+  def from_data(cls, data : dict) -> None:
+    return cls(data)
