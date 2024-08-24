2b38d85610d0ead33a2b6d76f2ac91892308aed1
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-08-24 16:11:37 +0800

    ApplicationFlags

---
+++ democord/flags.py
@@ -0,0 +1,8 @@
+class ApplicationFlags:
+  application_command_badge = 1 << 23
+  automod                   = 1 << 6
+  embedded                  = 1 << 17
+  members                   = 1 << 14 | 1 << 15
+  message_content           = 1 << 18 | 1 << 19
+  pending_verification      = 1 << 16
+  presence                  = 1 << 12 | 1 << 13
