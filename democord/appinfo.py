58d23d6adc3e63a90577ddc10627c98ab21a2aa2
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-08-24 19:08:09 +0800

    App.info.flags -> AppInfoFlags

--- democord/appinfo.py
+++ democord/appinfo.py
@@ -1,3 +1,6 @@
+from .flags import (
+  ApplicationFlags
+)
 from typing import (
   Self
 )
@@ -9,7 +12,9 @@ class AppInfo:
     attributes
   ) -> None:
     for attribute in attributes:
-      self.__dict__[attribute] = attributes[attribute]
+      match attribute:
+        case "flags": self.__dict__["flags"] = AppInfoFlags(attributes[attribute])
+        case _: self.__dict__[attribute] = attributes[attribute]
 
 
   def __getattribute__(self, attribute : str):
@@ -21,3 +26,14 @@ class AppInfo:
   @classmethod
   def from_data(cls, data) -> Self:
     return cls(data)
+
+
+class AppInfoFlags:
+  def __init__(self, bitfield : int) -> None:
+    self.value : int = bitfield
+
+
+  def __contains__(self, name : str) -> bool:
+    flag : ApplicationFlags = ApplicationFlags._member_map_.get(name)
+    if not flag: raise AttributeError(f"There is no such Application Flag named: {name}")
+    return ( self.value & flag.value ) == flag.value
