a70d576fdf1b74a08c815e275d1dc41241fe2370
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-08-27 22:15:15 +0800

    Allow registration of democord.on_guild_available events

--- democord/events.py
+++ democord/events.py
@@ -22,18 +22,23 @@ class AppEvents:
   ) -> None:
     self.app : App = app
     self.ready : list = [self.app.on_ready]
+    self.on_guild_available : list = [self.app.on_guild_available]
 
 
   def add(self, callback) -> None:
     match callback.__name__:
-      case "on_ready":
-        self.ready.append(callback)
+      case "on_ready": self.ready.append(callback)
+      case "on_guild_available": self.on_guild_available.append(callback)
 
 
-  async def call(self, event : GatewayEvents) -> None:
+  async def call(self, event : GatewayEvents, *, guild = None) -> None:
     tasks = []
     match event:
       case GatewayEvents.Ready:
         for callback in self.ready:
           tasks.append(callback())
+      case GatewayEvents.GuildCreate:
+        for callback in self.on_guild_available:
+          if guild not in self.app.guilds: self.app.guilds.append(guild)
+          else: tasks.append(callback(guild))
     await gather(*tasks)
