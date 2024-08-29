b7effbe6b2ca6422ad85805089ba21a6248c438f
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-08-29 17:50:54 +0800

    Color.from_rgb(red, green, blue) -> Color

--- democord/color.py
+++ democord/color.py
@@ -8,3 +8,10 @@ class Color:
     color.value : int = value
     color.hex : str = hex(value).replace("0x", "#")
     return color
+
+  @classmethod
+  def from_rgb(cls, red : int = 0, green : int = 0, blue : int = 0) -> Self:
+    color : Self = cls()
+    color.value : int = (red * 65536) + (green * 256) + blue
+    color.hex : str = hex(color.value).replace("0x", "#")
+    return color
