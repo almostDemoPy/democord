da97b9e6c68a70f26439ab6b079450eb36191b66
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-08-29 22:17:56 +0800

    democord.UserFlags

--- democord/flags.py
+++ democord/flags.py
@@ -12,3 +12,21 @@ class ApplicationFlags(Enum):
   pending_verification      = 1 << 16
   presence                  = 1 << 12
   presence_limited          = 1 << 13
+
+
+class UserFlags(Enum):
+  staff                = 1 << 0
+  partner              = 1 << 1
+  hypesquad_events     = 1 << 2
+  bug_hunter_level_1   = 1 << 3
+  hypesquad_bravery    = 1 << 6
+  hypesquad_brilliance = 1 << 7
+  hypesquad_balance    = 1 << 8
+  early_supporter      = 1 << 9
+  team                 = 1 << 10
+  bug_hunter_level_2   = 1 << 14
+  verified_bot         = 1 << 16
+  verified_developer   = 1 << 17
+  certified_moderator  = 1 << 18
+  http_bot             = 1 << 19
+  active_developer     = 1 << 22
