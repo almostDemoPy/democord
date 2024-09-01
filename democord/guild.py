daf136dcde26ff1976876191f6deba1c9394c83d
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-09-01 21:10:55 +0800

    Guild.explicit_content_filter -> str

--- democord/guild.py
+++ democord/guild.py
@@ -27,7 +27,7 @@ class Guild:
         case "id": self.__dict__[attribute] = int(data[attribute])
         case "owner_id": self.__dict__["owner"] = User.from_id(ws, data["owner_id"])
         case "default_message_notifications": self.__dict__[attribute] = DefaultMessageNotification(data[attribute]).name
-        case "explicit_content_filter": self.__dict__[attribute] = ExplicitContentFilter(data[attribute])
+        case "explicit_content_filter": self.__dict__[attribute] = ExplicitContentFilter(data[attribute]).name
         case "mfa_level": self.__dict__[attribute] = MFALevel(data[attribute]).name
         case "system_channel_flags": self.__dict__[attribute] = CallableSystemChannelFlags(name for name, flag in SystemChannelFlags._member_map_.items() if (data[attribute] & flag.value) == flag.value)
         case "premium_tier": self.__dict__[attribute] = PremiumTier(data[attribute]).name
