56061b2f82cb24f2964c01a63acaac07b017f5e9
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-09-01 18:29:25 +0800

    Guild.mfa_level -> MFALevel[str]

--- democord/guild.py
+++ democord/guild.py
@@ -27,7 +27,7 @@ class Guild:
         case "owner_id": self.__dict__["owner"] = User.from_id(ws, data["owner_id"])
         case "default_message_notifications": self.__dict__[attribute] = DefaultMessageNotification(data[attribute])
         case "explicit_content_filter": self.__dict__[attribute] = ExplicitContentFilter(data[attribute])
-        case "mfa_level": self.__dict__[attribute] = MFALevel(data[attribute])
+        case "mfa_level": self.__dict__[attribute] = MFALevel(data[attribute]).name
         case "system_channel_flags": self.__dict__[attribute] = CallableSystemChannelFlags(name for name, flag in SystemChannelFlags._member_map_.items() if (data[attribute] & flag.value) == flag.value)
         case "premium_tier": self.__dict__[attribute] = PremiumTier(data[attribute]).name
         case _: self.__dict__[attribute] = data[attribute]
