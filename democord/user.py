4fa09907fad43e3b62009cf4c36b50d0e160512a
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-09-03 21:25:23 +0800

    Fixed error of not being able to append the instance variables when avatar is None

--- democord/user.py
+++ democord/user.py
@@ -34,9 +34,9 @@ class User:
           else: user.__dict__[attribute] = data[attribute]
         case "banner": user.__dict__[attribute] : Asset = Asset.from_user(attribute, data) if data[attribute] else None
         case "avatar":
-          user.__dict__[attribute] : str = CallableAvatar(f"https://cdn.discordapp.com/avatars/{data["id"]}/{data["avatar"]}.{"gif" if data["avatar"].startswith("a_") else "png"}") if data[attribute] else None
-          user.__dict__[attribute].default_avatar : str = f"https://cdn.discordapp.com/embed/avatars/{(int(data["id"]) >> 22) % 6 if int(data["discriminator"]) == 0 else int(data["discriminator"]) % 5}.png"
-          user.__dict__[attribute].avatar_decoration : str = f"https://cdn.discordapp.com/avatar-decoration-presets/{data["avatar_decoration_data"]["asset"]}.png" if data["avatar_decoration_data"] else ""
+          user.__dict__[attribute] : str = CallableAvatar(f"https://cdn.discordapp.com/avatars/{data["id"]}/{data["avatar"]}.{"gif" if data["avatar"].startswith("a_") else "png"}") if data[attribute] else ""
+          user.default_avatar : str = f"https://cdn.discordapp.com/embed/avatars/{(int(data["id"]) >> 22) % 6 if int(data["discriminator"]) == 0 else int(data["discriminator"]) % 5}.png"
+          user.avatar_decoration : str = f"https://cdn.discordapp.com/avatar-decoration-presets/{data["avatar_decoration_data"]["asset"]}.png" if data.get("avatar_decoration_data") else ""
         case "accent_color": user.__dict__[attribute] : Color = Color.from_int(data[attribute])
         case "locale": user.__dict__[attribute] : Locale = Locale._value2member_map_[data[attribute]]
         case "flags": user.__dict__[attribute] : list = [name for name, flag in UserFlags._member_map_.items() if (data[attribute] & flag.value) == flag.value]
