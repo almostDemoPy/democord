1ae39c94b65d49743890d055d2bd871402e0457f
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-09-04 20:42:30 +0800

    Member.user -> Optional[User]

--- democord/member.py
+++ democord/member.py
@@ -6,4 +6,7 @@ class Member:
   @classmethod
   def from_data(cls, ws, data : dict) -> Self:
     member : Self = cls()
+    for attribute in data:
+      match attribute:
+        case "user": member.__dict__[attribute] = User.from_data(data)
     return member
