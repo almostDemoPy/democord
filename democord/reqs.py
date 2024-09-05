b4b4cb2e52f66cae3e2329c4808f340ddb337f84
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-09-05 14:39:02 +0800

    PATCH.guild(guild_id : int)

--- democord/reqs.py
+++ democord/reqs.py
@@ -4,3 +4,7 @@ from enum import Enum
 class GET(Enum):
   gateway = "/gateway"
   guild   = lambda guild_id, with_counts: f"/guilds/{guild_id}?with_counts={with_counts}"
+
+
+class PATCH(Enum):
+  guild = lambda guild_id: f"/guilds/{guild_id}"
