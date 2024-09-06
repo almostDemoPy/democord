f780e84d7d6bf10dc94f13807dd3755593487ddc
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-09-06 23:30:28 +0800

    Logger.info(message : str) -> None

--- democord/logger.py
+++ democord/logger.py
@@ -8,3 +8,7 @@ class Logger:
     self
   ) -> None:
     self._format : str = "%Y-%m-%d %H:%M:%S.%f"
+
+  def info(self, message : str) -> None:
+    current_time : datetime = datetime.now()
+    print(f"[ {current_time.strftime(self._format)} ] INFO | {message}".replace("\n", f"\n{" " * (len(current_time.strftime(self._format)) + 10)}| "))
