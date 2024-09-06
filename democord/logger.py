b098785c1f2116c10edfb1e25bcd9d561c1ad7c3
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-09-07 00:24:38 +0800

    Logger.error() ; Logger.warn() ; Logger.debug()

--- democord/logger.py
+++ democord/logger.py
@@ -1,3 +1,7 @@
+from colorama import (
+  Fore,
+  Style
+)
 from datetime import (
   datetime,
   timedelta
@@ -7,10 +11,42 @@ class Logger:
   def __init__(
     self,
     *,
-    _format : str = "%Y-%m-%d %H:%M:%S"
+    time_format : str = "%Y-%m-%d %H:%M:%S"
   ) -> None:
-    self._format : str = _format
+    self._format : str = time_format
+    self.max_type_length : int = 7
+
+  def _log(self, *, log_type : str, message : str) -> None:
+    log_colors : dict[str, str] = {
+      "INFO"   : Fore.CYAN,
+      "ERROR"  : Fore.RED,
+      "WARNING": Fore.YELLOW,
+      "DEBUG"  : Fore.YELLOW
+    }
+    timestamp = datetime.now().strftime(self._format)
+    message : str = message.replace("\n", f"\n{" " * (len(timestamp) + 13)}| ")
+    print(f"[ {timestamp} ] {log_colors[log_type]}{"{0:>{1}}".format(log_type, self.max_type_length)}{Style.RESET_ALL} | {message}")
 
   def info(self, message : str) -> None:
-    current_time : datetime = datetime.now()
-    print(f"[ {current_time.strftime(self._format)} ] INFO | {message}".replace("\n", f"\n{" " * (len(current_time.strftime(self._format)) + 10)}| "))
+    self._log(
+      log_type = "INFO",
+      message = message
+    )
+
+  def error(self, message : str) -> None:
+    self._log(
+      log_type = "ERROR",
+      message = message
+    )
+
+  def warn(self, message : str) -> None:
+    self._log(
+      log_type = "WARNING",
+      message = message
+    )
+
+  def debug(self, message : str) -> None:
+    self._log(
+      log_type = "DEBUG",
+      message = message
+    )
