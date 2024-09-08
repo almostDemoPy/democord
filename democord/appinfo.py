f0c4853f714ef97c44ad82855644728eb5efc8c6
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-09-08 21:01:40 +0800

    democord/appinfo.py : Added docstrings and type hints

--- democord/appinfo.py
+++ democord/appinfo.py
@@ -1,13 +1,22 @@
 from .flags import (
   ApplicationFlags
 )
-from typing import (
-  Self
-)
+from typing import *
 
 
 class AppInfoFlags(int):
-  def __call__(self) -> list[str]:
+  """
+  The application's flags
+  """
+
+  def __call__(self) -> List[str]:
+    """
+    Returns the application's flags in strings of names
+
+    Returns
+    -------
+    List[str]
+    """
     return [
       name
       for name, flag in ApplicationFlags._member_map_.items()
@@ -15,18 +24,108 @@ class AppInfoFlags(int):
     ]
 
   def __int__(self) -> int:
+    """
+    The bitfield value of the application's flags
+    """
     return self
 
 
 class AppInfo:
+  """
+  The application's info data
+
+
+  Attributes
+  ----------
+  approximate_guild_count : Optional[int]
+    Approximate count of guilds the bot is added to
+
+  approximate_user_install_count : Optional[int]
+    Approximate count of users that installed the application
+
+  bot : Optional[User]
+    User object of the application
+
+  cover_image : Optional[Asset]
+    Applicaiton's default rich presence invite
+
+  custom_install_url : Optional[str]
+    Default custom authorization URL for the application
+
+  description : str
+    Description of the application
+
+  flags : Union[int, List[str]]
+    The application's flag info
+
+  guild : Optional[Guild]
+    The guild associated with the app.
+
+  icon : Optional[Asset]
+    Set icon for the application
+
+  id : int
+    Application ID
+
+  install_params : Optional[Dict[str, List[str]]]
+    Set install parameters for the application's default in-app authorization link
+
+  integration_types_config : Optional[Tuple[IntegrationType]]
+    Set integration types for the application
+
+  interactions_endpoint_url : Optional[str]
+    Interactions endpoint URL for the application
+
+  name : str
+    Name of the application
+
+  owner : Optional[User]
+    The application's owner
+
+  primary_sku_id : Optional[int]
+    The ID of the "Game SKU" that is created, if exists
+
+  privacy_policy_url : Optional[str]
+    URL of the application's Privacy Policy
+
+  public : bool
+    Whether the bot can be added by anyone or only the application owner
+
+  redirect_uris : Optional[List[str]]
+    Redirect URIs for the application
+
+  require_code_grant : bool
+    Whether the bot requires OAuth2 Code grant when adding
+
+  role_connections_verification_url : Optional[str]
+    Role connections verification URL for the application
+
+  slug : Optional[str]
+    The URL slug that links to the store page
+
+  tags : Optional[List[str]]
+    List of tags describing the application
+
+  team : Optional[Team]
+    The Team object the application belongs in
+
+  tos_url : Optional[str]
+    URL of the application's Terms of Service
+
+  verify_key : str
+    Hex encoded key for verification in interactions and the GameSDK's GetTicket
+  """
+
   def __init__(
     self,
-    attributes
+    attributes : Dict[str, Any]
   ) -> None:
     for attribute in attributes:
       match attribute:
-        case "flags": self.__dict__["flags"] = AppInfoFlags(attributes[attribute])
-        case _: self.__dict__[attribute] = attributes[attribute]
+        case "flags": self.__dict__["flags"] : Union[int, List[str]] = AppInfoFlags(attributes[attribute])
+        case "bot_public": self.__dict__["public"] : bool = attributes[attribute]
+        case "bot_require_code_grant": self.__dict__["require_code_grant"] : bool = attributes[attribute]
+        case _: self.__dict__[attribute] : Any = attributes[attribute]
 
 
   def __getattribute__(self, attribute : str):
@@ -47,5 +146,5 @@ class AppInfo:
 
 
   @classmethod
-  def from_data(cls, data) -> Self:
+  def from_data(cls, data : Dict[str, Any]) -> Self:
     return cls(data)
