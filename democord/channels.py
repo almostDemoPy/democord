79ac2ddd7d3492f60e99c20638f11948973e6264
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-09-08 21:11:27 +0800

    democord/channels.py : Added docstrings and type hints

--- democord/channels.py
+++ democord/channels.py
@@ -1,1 +1,9 @@
-class GuildChannel: pass
+from typing import *
+
+
+class GuildChannel:
+  """
+  Represents a guild channel. This can be further classified as TextChannel, VoiceChannel, ForumChannel, StageChannel, and Thread, when subclassed.
+  """
+
+  pass
