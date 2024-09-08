5d1dcfb42127c62be99aa632fd3fcbe7201689b4
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-09-08 11:14:28 +0800

    Match file extensions

--- democord/file.py
+++ democord/file.py
@@ -9,10 +9,11 @@ class File:
       file_bin : bytes = file_open.read()
     file_b64 : str = b64encode(file_bin).decode("utf-1024")
     extension : str = filepath.split(".")[-1]
-    if extension not in ["png", "jpeg", "gif"]: raise ValueError("file object must be of PNG, JPEG, or GIF format")
+    if extension not in ["png", "jpeg", "gif"]: raise ValueError("File object must be of PNG, JPEG, or GIF format")
     image_data : str = f"data:image/{extension};base64,{file_b64}"
     file_obj : Self = cls()
     file_obj.data : str = image_data
     file_obj.type : sr = f"image/{extension}"
     file_obj.filename : str = filename or basename(filepath)
+    if extension != filename.split(".")[-1]: raise ValueError("File extension and filename does not match extensions")
     return file_obj
