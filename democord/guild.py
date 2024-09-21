c392a8cf0985d509967441818601fe43fc7a2988
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-09-21 12:50:45 +0800

    await Guild.edit(rules_channel : Optional[GuildChannel]) -> Guild

--- democord/guild.py
+++ democord/guild.py
@@ -304,6 +304,11 @@ class Guild:
           if not isinstance(attributes[attribute], User): raise TypeError("Guild.owner must be of type <User>")
           data[attribute] : int = attributes[attribute].id
 
+        case "rules_channel":
+          if not isinstance(attributes[attribute], (GuildChannel, None)):
+            raise TypeError("Guild.rules_channel must be of type <GuildChannel> or <NoneType>")
+          data[attribute] : int | None = int(attributes[attribute]) if attributes[attribute] else None
+
         case "system_channel":
           if not isinstance(attributes[attribute], (GuildChannel, None)):
             raise TypeError("Guild.system_channel must be of type <GuildChannel> or <NoneType>")
