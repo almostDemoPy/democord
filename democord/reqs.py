f8ee9fa6888c8b3cbbb49c1c325d3668ec8b7169
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-10-20 17:52:23 +0800

    PUT.channel_permissions(channel_id, overwrites_id) -> str

--- democord/reqs.py
+++ democord/reqs.py
@@ -38,5 +38,6 @@ class POST(Enum):
   guild : str = "/guilds"
 
 class PUT(Enum):
-  member : Callable[[int, int], str] = lambda guild_id, user_id : f"/guilds{guild_id}/members/{user_id}"
+  channel_permissions : Callable[[int, int], str] = lambda channel_id, overwrites_id : f"/channels/{channel_id}/permissions/{overwrite_id}"
+  member : Callable[[int, int], str] = lambda guild_id, user_id : f"/guilds/{guild_id}/members/{user_id}"
   member_role : Callable[[int, int, int], str] = lambda guild_id, member_id, role_id : f"/guilds/{guild_id}/members/{member_id}/roles/{role_id}"
